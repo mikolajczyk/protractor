0ab73bea295eed079bb314a39e630bfd4d530d1f
Matt <matt.mikolajczyk1@gmail.com>
2021-03-30 08:47:06 -0700

    Add firefox/chrome support

--- conf.js
+++ conf.js
@@ -2,5 +2,10 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    specs: ['spec.js'],
+    multiCapabilites: [{
+      browserName: 'firefox'
+    }, {
+      browserName: 'chrome'
+    }]
   }
